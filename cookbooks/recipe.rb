c236e9252defe58bb5c45c2a2a93453ff2f04526
Ikuo Degawa <degawa@ikuwow.com>
2016-08-11 23:22:07 +0900

    Add php_config resource test

--- cookbooks/recipe.rb
+++ cookbooks/recipe.rb
@@ -7,3 +7,14 @@ remote_file '/var/www/html/index.html' do
     owner 'apache'
     group 'apache'
 end
+
+%w{php php-devel php-mbstring php-gd}.each do |pkg|
+    package pkg
+end
+
+file '/etc/php.ini' do
+    action :edit
+    block do |content|
+        content.gsub!(/^;date.timezone =$/, 'date.timezone = Asia/Tokyo')
+    end
+end
---
+++ spec/serverspec-prac/php_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+%w{php php-devel php-mbstring php-gd}.each do |pkg|
+    describe package(pkg) do
+        it { should be_installed }
+    end
+end
+
+describe php_config('date.timezone') do
+    its(:value) { should eq 'Asia/Tokyo' }
+end
