1e06dd0f19783ac8e3b829ae2e3bfb4959e573d9
Ikuo Degawa <degawa@ikuwow.com>
2016-08-11 22:39:00 +0900

    Add port test

--- spec/serverspec-prac/httpd_spec.rb
+++ spec/serverspec-prac/httpd_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
-describe package('httpd') do
-    it { should be_installed }
+describe package('httpd') do # resource
+    it { should be_installed } # matcher
 end
 
 describe service('httpd') do
     it { should be_enabled }
     it { should be_running }
+end
+
+describe port(80) do
+    it { should be_listening }
 end
